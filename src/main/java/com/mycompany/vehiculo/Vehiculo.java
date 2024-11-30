6c5e312186a9c6a0e61845555a7344ae9c7c7e25
ShellDomenic <domenicaloor177@gmail.com>
2024-11-30 18:14:26 -0500

    hola!

---
+++ src/main/java/com/mycompany/vehiculo/Vehiculo.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.vehiculo;
+
+/**
+ *
+ * @author and_j
+ */
+public class Vehiculo {
+    
+    
+    
+    
+   
+    }
+}
+    
+
